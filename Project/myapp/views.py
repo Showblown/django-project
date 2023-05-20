5aa851de468ae68007f76ea7573d81dc38de4fca
Showblown <showblown@gmail.com>
2023-05-20 09:52:23 -0700

    my second commit

--- Project/myapp/views.py
+++ Project/myapp/views.py
@@ -3,13 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 def hello_django(request):
-    print("Printing Request")
     print(request) 
     # while True:
     #     print("Hello")
+    # the browser will keep loading ths tab if the above code is uncommented
     return HttpResponse("Hello django")
 
 def home(request):
     return render(request,"myapp/home.html")
 
-#https://github.com/Showblown/django-project
+#https://github.com/Showblown/django-projec
